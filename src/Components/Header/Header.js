b1536614caddc816732231a5de8f9fbe5d2fda4e
alig4t <ali.gha3mi75@mail.com>
2023-05-15 16:22:33 +0330

    initialize todo App

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="row">
+            <div className="col-lg-12 text-center">
+                <h1>To Do App</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
